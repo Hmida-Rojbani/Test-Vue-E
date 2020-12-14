707289df28c0f1f4b5f518c186b0eb59efe0434f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-14 15:47:48 +0100

    Add gender

--- src/main/java/de/tekup/vue/dto/models/User.java
+++ src/main/java/de/tekup/vue/dto/models/User.java
@@ -1,14 +1,23 @@
 package de.tekup.vue.dto.models;
 
+import java.time.LocalDate;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter@Setter
+@Getter
+@Setter
 @ToString
 public class User {
 
 	private String name;
 	private String email;
 	private String password;
+	// add theses fields to HTML
+	private String gender;
+	private String note;
+	private boolean married;
+	private LocalDate birthDate;
+	private String profession;
 }
